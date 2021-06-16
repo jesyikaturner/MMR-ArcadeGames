d14fe6d64f1ceacfe6185a5a88c5aa7d28d911f7
Jessica Turner <jesyika@gmail.com>
2021-06-16 17:36:02 +1000

    init commit

---
+++ Snek/Assets/SnakeBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeBehaviour : MonoBehaviour
+{
+    private const float Grid_Move_Timer_Max = 1f;
+
+    private Vector2Int gridMoveDirection;
+    private Vector2Int gridPosition;
+    private float gridMoveTimer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        gridPosition = new Vector2Int(10, 10);
+        gridMoveTimer = Grid_Move_Timer_Max;
+        gridMoveDirection = Vector2Int.up;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (v > 0)
+            gridMoveDirection = Vector2Int.up;
+        else if (v < 0)
+            gridMoveDirection = Vector2Int.down;
+        else if (h > 0)
+            gridMoveDirection = Vector2Int.right;
+        else if (h < 0)
+            gridMoveDirection = Vector2Int.left;
+
+        gridMoveTimer += Time.deltaTime;
+
+        if (gridMoveTimer >= Grid_Move_Timer_Max)
+        {
+            gridPosition += gridMoveDirection;
+            gridMoveTimer -= Grid_Move_Timer_Max;
+        }
+
+        transform.position = new Vector2(gridPosition.x, gridPosition.y);
+        transform.eulerAngles = new Vector3(0, 0, GetAngleFromVector(gridMoveDirection) - 90);
+    }
+
+    private float GetAngleFromVector(Vector2Int dir)
+    {
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        return n;
+    }
+}
